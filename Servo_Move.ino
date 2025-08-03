4cbb4f8427503767f38e4c580526cd53ad5892e1
shoyaa0815 <shoyaa0815@gmail.com>
2025-08-03 19:32:26 +0700

    Update Servo_Move.ino

--- Servo_Move.ino
+++ Servo_Move.ino
@@ -1,128 +1,128 @@
-void MoveFw() {
+void MoveFw(int SpeedDelay) {
   L_F_3_servo.write(60); 
   R_M_3_servo.write(120);
   L_B_3_servo.write(60);
-  delay(200);
+  delay(SpeedDelay);
 
   L_F_1_servo.write(60);
   R_M_1_servo.write(120);
   L_B_1_servo.write(60);
-  delay(200);
+  delay(SpeedDelay);
 
   L_F_3_servo.write(90);
   R_M_3_servo.write(90);
   L_B_3_servo.write(90);
-  delay(200);
+  delay(SpeedDelay);
 
   R_F_3_servo.write(120);
   L_M_3_servo.write(60);
   R_B_3_servo.write(120);
-  delay(200);
+  delay(SpeedDelay);
 
   R_F_1_servo.write(120);
   L_M_1_servo.write(60);
   R_B_1_servo.write(120);
-  delay(200);
+  delay(SpeedDelay);
   
   R_F_3_servo.write(90);
   L_M_3_servo.write(90);
   R_B_3_servo.write(90);
-  delay(200);
+  delay(SpeedDelay);
 }
 
-void MoveBw() {  
+void MoveBw(int SpeedDelay) {  
   L_F_3_servo.write(60);
   R_M_3_servo.write(120);
   L_B_3_servo.write(60);
-  delay(200);
+  delay(SpeedDelay);
 
   L_F_1_servo.write(120);
   R_M_1_servo.write(60);
   L_B_1_servo.write(120);
-  delay(200);
+  delay(SpeedDelay);
 
   L_F_3_servo.write(90);
   R_M_3_servo.write(90);
   L_B_3_servo.write(90);
-  delay(200);
+  delay(SpeedDelay);
 
   R_F_3_servo.write(120);
   L_M_3_servo.write(60);
   R_B_3_servo.write(120);
-  delay(200);
+  delay(SpeedDelay);
 
   R_F_1_servo.write(60);
   L_M_1_servo.write(120);
   R_B_1_servo.write(60);
-  delay(200);
+  delay(SpeedDelay);
 
   R_F_3_servo.write(90);
   L_M_3_servo.write(90);
   R_B_3_servo.write(90);
-  delay(200);
+  delay(SpeedDelay);
 }
 
-void SlideL() {  
+void SlideL(int SpeedDelay) {  
   L_F_3_servo.write(60);
   L_M_3_servo.write(60);
   L_B_3_servo.write(60);
-  delay(200);
+  delay(SpeedDelay);
 
   R_F_1_servo.write(120);
   R_M_1_servo.write(120);
   R_B_1_servo.write(120);
-  delay(200);
+  delay(SpeedDelay);
 
   L_F_3_servo.write(90);
   L_M_3_servo.write(90);
   L_B_3_servo.write(90);
-  delay(200);
+  delay(SpeedDelay);
 
   R_F_3_servo.write(120);
   R_M_3_servo.write(120);
   R_B_3_servo.write(120);
-  delay(200);
+  delay(SpeedDelay);
 
   L_F_1_servo.write(60);
   L_M_1_servo.write(60);
   L_B_1_servo.write(60);
-  delay(200);
+  delay(SpeedDelay);
 
   R_F_3_servo.write(90);
   R_M_3_servo.write(90);
   R_B_3_servo.write(90);
-  delay(200);
+  delay(SpeedDelay);
 }
 
 
-void SlideR() {
+void SlideR(int SpeedDelay) {
   R_F_3_servo.write(120);
   R_M_3_servo.write(120);
   R_B_3_servo.write(120);
-  delay(200);
+  delay(SpeedDelay);
 
   L_F_1_servo.write(60);
   L_M_1_servo.write(60);
   L_B_1_servo.write(60);
-  delay(200);
+  delay(SpeedDelay);
 
   R_F_3_servo.write(90);
   R_M_3_servo.write(90);
   R_B_3_servo.write(90);
-  delay(200);
+  delay(SpeedDelay);
   
   L_F_3_servo.write(60);
   L_M_3_servo.write(60);
   L_B_3_servo.write(60);
-  delay(200);
+  delay(SpeedDelay);
 
   R_F_1_servo.write(120);
   R_M_1_servo.write(120);
   R_B_1_servo.write(120);
-  delay(200);
+  delay(SpeedDelay);
   
   L_F_3_servo.write(90);
   L_M_3_servo.write(90);
   L_B_3_servo.write(90);
-  delay(200);
+  delay(SpeedDelay);
 }
